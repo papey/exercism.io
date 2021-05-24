bab40cc3be4f5fce300546cf7a18229587ae8790
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-05-24 10:03:26 +0200

    Ruby scrabble score the other way

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,11 +1,11 @@
 SCORING = {
-  'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1, 'N' => 1, 'R' => 1, 'S' => 1, 'T' => 1,
-  'D' => 2, 'G' => 2,
-  'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3,
-  'F' => 4, 'H' => 4, 'V' => 4, 'W' => 4, 'Y' => 4,
-  'K' => 5,
-  'J' => 8, 'X' => 8,
-  'Q' => 10, 'Z' => 10
+  /[AEIOULNRST]/ => 1,
+  /[DG]/ => 2,
+  /[BCMP]/ => 3,
+  /[FHVWY]/ => 4,
+  /K/ => 5,
+  /[JX]/ => 8,
+  /[QZ]/ => 10
 }.freeze
 
 class Scrabble
@@ -20,6 +20,6 @@ class Scrabble
   end
 
   def score
-    @score ||= word.chars.sum { |letter| SCORING[letter] }
+    @score ||= SCORING.each.sum { |(regex, value)| word.scan(regex).count * value }
   end
 end
