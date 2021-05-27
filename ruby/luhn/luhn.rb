a38c0f05ceabf1c9cd0c33468fe12f45411c598f
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-05-27 18:05:55 +0200

    Add ruby luhn

---
+++ ruby/luhn/luhn.rb
@@ -0,0 +1,44 @@
+module LuhnableString
+  refine String do
+    private
+
+    def valid_length?(id_number)
+      id_number.length > 1
+    end
+
+    def digits_only?(id_number)
+      !id_number.match(/\D/)
+    end
+
+    def valid_sum?(id_number)
+      (sum(id_number) % 10).zero?
+    end
+
+    def sum(id_number)
+      digits = id_number.chars.map(&:to_i).reverse
+      digits.map.with_index do |digit, index|
+        index.odd? ? multiply(digit) : digit
+      end.reduce(:+)
+    end
+
+    def multiply(digit)
+      value = digit * 2
+      value > 9 ? value - 9 : value
+    end
+
+    public
+
+    def valid_luhn?
+      id_number = gsub(/\s+/, '')
+      valid_length?(id_number) && digits_only?(id_number) && valid_sum?(id_number)
+    end
+  end
+end
+
+module Luhn
+  using LuhnableString
+
+  def self.valid?(id_number)
+    id_number.valid_luhn?
+  end
+end
