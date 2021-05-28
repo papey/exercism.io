46b639f63bd9a6a46dec2c757b10ae40a5674247
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-05-28 08:51:25 +0200

    Update nim isogram

--- nim/isogram/isogram.nim
+++ nim/isogram/isogram.nim
@@ -1,10 +1,8 @@
-import strutils
-import sequtils
-import sugar
+import strutils, sequtils, sugar
 
 proc isIsogram*(word : string): bool =
   let letters = collect(newSeq):
-    for x in @word:
+    for x in word:
       if isAlphaAscii(x):
         x.toLowerAscii()
 
