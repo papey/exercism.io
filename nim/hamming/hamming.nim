7c4e80c1c9573972faf5d332beedc0175c5a08f7
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-06-09 09:48:35 +0200

    Update nim hamming

--- nim/hamming/hamming.nim
+++ nim/hamming/hamming.nim
@@ -4,4 +4,4 @@ proc distance*(strandA : string, strandB : string): int =
     if len(strandA) != len(strandB):
         raise newException(ValueError, "Distance needs a strand of same length")
 
-    result = zip(@strandA, @strandB).filterIt(it[0] != it[1]).len
+    result = zip(strandA, strandB).filterIt(it[0] != it[1]).len
