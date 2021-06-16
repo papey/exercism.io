d9464695d6f931d99daed3f7f435cf56e352e163
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-06-16 10:45:39 +0200

    Add rust high scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores: scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().cloned()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().cloned()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        // acquire cloned mutable data to sort without modifying struct data
+        let mut top = self.scores().to_vec();
+        // do sort
+        top.sort_unstable();
+        // do reverse
+        top.reverse();
+        top.into_iter().take(3).collect()
+    }
+}
