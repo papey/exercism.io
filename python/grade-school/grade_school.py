1add29a479df7a8f84914a749492648b2ce17b22
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-06-22 11:23:54 +0200

    Update python grade school

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,15 +1,15 @@
-from typing import DefaultDict
+from collections import defaultdict
 
 
 class School:
     def __init__(self):
-        self.data = DefaultDict(set)
+        self.data = defaultdict(list)
 
     def add_student(self, name, grade):
-        self.data[grade].add(name)
+        self.data[grade].append(name)
 
     def roster(self):
         return [name for k in sorted(self.data.keys()) for name in sorted(self.data[k])]
 
     def grade(self, grade_number):
-        return sorted(list(self.data[grade_number])) or []
+        return sorted(self.data[grade_number])
