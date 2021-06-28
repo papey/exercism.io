5f8c595205ef91d8d1588ac138345b8cdaaebbcb
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-06-28 08:41:13 +0200

    Less verbose

--- nim/sum-of-multiples/sum_of_multiples.nim
+++ nim/sum-of-multiples/sum_of_multiples.nim
@@ -1,5 +1,4 @@
-import sequtils, sugar
+import sequtils, sugar, math
 
 func sum*(n: int, factors: seq[int]): int =
-  let validFactors = factors.filterIt(it != 0)
-  toSeq(1..<n).filter(x => validFactors.anyIt(x mod it == 0)).foldl(a + b, 0)
+  toSeq(1..<n).filter(x => factors.anyIt(it != 0 and x mod it == 0)).sum
