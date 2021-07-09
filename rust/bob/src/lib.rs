b496c114c29409679b5d7b2137ce8852a4ad18e8
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-07-09 16:44:02 +0200

    More work on rust bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,5 +1,3 @@
-const QUESTION_MARK: char = '?';
-
 const SHOUTING_REPLY: &str = "Whoa, chill out!";
 const DEFAULT_REPLY: &str = "Whatever.";
 const QUESTION_REPLY: &str = "Sure.";
@@ -8,16 +6,18 @@ const SOUTHING_QUESTION_REPLY: &str = "Calm down, I know what I'm doing!";
 
 pub fn reply(message: &str) -> &str {
     let sanitized = message.trim();
-    let is_question = sanitized.ends_with(QUESTION_MARK);
+    let is_question = sanitized.ends_with('?');
     let is_shouting =
         sanitized.chars().any(char::is_alphabetic) && message == message.to_uppercase();
-    let is_silence = sanitized.is_empty();
 
-    match (is_question, is_shouting, is_silence) {
-        (_, _, true) => SILENCE_REPLY,
-        (true, true, _) => SOUTHING_QUESTION_REPLY,
-        (_, true, _) => SHOUTING_REPLY,
-        (true, _, _) => QUESTION_REPLY,
+    if sanitized.is_empty() {
+        return SILENCE_REPLY;
+    }
+
+    match (is_question, is_shouting) {
+        (true, true) => SOUTHING_QUESTION_REPLY,
+        (_, true) => SHOUTING_REPLY,
+        (true, _) => QUESTION_REPLY,
         _ => DEFAULT_REPLY,
     }
 }
