760a81706723941702782b84523805f073db7d56
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-07-11 18:25:04 +0200

    Move early return early in rust bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -6,13 +6,13 @@ const SOUTHING_QUESTION_REPLY: &str = "Calm down, I know what I'm doing!";
 
 pub fn reply(message: &str) -> &str {
     let sanitized = message.trim();
-    let is_question = sanitized.ends_with('?');
-    let is_shouting =
-        sanitized.chars().any(char::is_alphabetic) && message == message.to_uppercase();
-
     if sanitized.is_empty() {
         return SILENCE_REPLY;
     }
+
+    let is_question = sanitized.ends_with('?');
+    let is_shouting =
+        sanitized.chars().any(char::is_alphabetic) && message == message.to_uppercase();
 
     match (is_question, is_shouting) {
         (true, true) => SOUTHING_QUESTION_REPLY,
