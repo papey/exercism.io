c58cd7aa9cfd0d1748cba370cf38b5287e3f449a
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-08-11 15:56:47 +0200

    Python, add markdown

---
+++ python/markdown/markdown.py
@@ -0,0 +1,34 @@
+import re
+
+BOLD = re.compile(r"__(.*?)__")
+ITALIC = re.compile(r"_(.*?)_")
+TITLE = re.compile(r"^(#{1,6}) (.*)")
+OPENING_LIST = "<ul>"
+CLOSING_LIST = "</ul>"
+
+
+def parse(markdown):
+    markdown = re.sub(BOLD, r"<strong>\1</strong>", markdown)
+    markdown = re.sub(ITALIC, r"<em>\1</em>", markdown)
+
+    result = []
+    for line in markdown.split("\n"):
+        title = re.match(TITLE, line)
+        if title:
+            result.append(
+                "<h{level}>{content}</h{level}>".format(
+                    level=len(title.group(1)), content=title.group(2)
+                )
+            )
+        elif line.startswith("* "):
+            item = "<li>{item}</li>".format(item=line[2:])
+            if result and result[-1] == CLOSING_LIST:
+                result[-1] = item
+            else:
+                result.append(OPENING_LIST)
+                result.append(item)
+            result.append(CLOSING_LIST)
+        else:
+            result.append("<p>{line}</p>".format(line=line))
+
+    return "".join(result)
