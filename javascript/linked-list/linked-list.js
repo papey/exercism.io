876f08865b840dd232b88bd4371ab10ceec633f7
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-08-17 15:00:00 +0200

    JS, improved linked-list

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -1,16 +1,14 @@
 export class LinkedList {
   constructor() {
     this._len = 0;
-    this.head = null;
-    this.tail = null;
+    this.reset();
   }
 
   push(value) {
     const n = new Node(value);
 
     if (!this._len) {
-      this.head = n;
-      this.tail = n;
+      this.head = this.tail = n;
       this._len++;
       return;
     }
@@ -21,21 +19,9 @@ export class LinkedList {
     this._len++;
   }
 
-  pop() {
-    const n = this.tail;
-    this.tail = this.tail.prev;
-    this._len--;
+  pop = () => { return this.remove(this.tail) }
 
-    return n.value;
-  }
-
-  shift() {
-    const n = this.head;
-    this.head = n.next;
-    this._len--;
-
-    return n.value;
-  }
+  shift = () => { return this.remove(this.head) }
 
   unshift(value) {
     const n = new Node(value);
@@ -45,43 +31,57 @@ export class LinkedList {
   }
 
   delete(value) {
-    // if head if the value, remove it
-    if (this.head.value == value) {
-      this.shift();
-      return;
-    }
-
-    // if tail is the value, remove it
-    if (this.tail.value == value) {
-      this.pop();
-      return;
-    }
-
     let current = this.head;
-
-    while (current.next) {
-      if (current.next && current.next.value == value) {
-        current.next = current.next.next;
-        if (current.next) {
-          current.next.prev = current;
-        }
-        this._len--;
-        return;
-      }
+    while (current && current.value != value) {
       current = current.next;
     }
 
+    if (!current) {
+      return;
+    }
+
+    return this.remove(current);
+  }
+
+  remove(node) {
+    this._len--;
+    if (this._len == 0) {
+      this.reset();
+    }
+
+    if (node == this.head) {
+      this.head = this.head.next;
+      this.head.prev = null;
+    }
+
+    if (node == this.tail) {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    }
+
+    if (node.prev) {
+      node.prev.next = node.next;
+    }
+
+    if (node.next) {
+      node.next.prev = node.prev;
+    }
+
+    return node.value;
   }
 
   count() {
     return this._len;
+  }
+
+  reset() {
+    this.head = this.tail = null;
   }
 }
 
 class Node {
   constructor(value) {
     this.value = value;
-    this.prev = null;
-    this.next = null;
+    this.prev = this.next = null;
   }
 }
