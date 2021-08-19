5436da4b2985e32f5a6c4c08360ec377b7c7c963
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-08-19 09:08:04 +0200

    JS, simplify grade-school

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -1,33 +1,28 @@
 export class GradeSchool {
   constructor() {
     this._db = {};
-    this._students = new Set();
   }
 
-  roster = () => { return this.copy(this._db) }
+  roster = () => {
+    return this.copy(this._db);
+  };
 
-  add = (name, grade) => {
-    if (this._students.has(name)) {
-      this.remove(name);
-      this._students.delete(name);
-      return;
-    }
+  add(name, grade) {
+    Object.entries(this._db).forEach(([grade, students]) => {
+      if (students.includes(name))
+        this._db[grade].splice(students.indexOf(name));
+    });
 
-    this._students.add(name);
-    this._db[grade] ? this._db[grade].push(name) : this._db[grade] = [name];
+    this._db[grade] = this._db[grade] || [];
+    this._db[grade].push(name);
     this._db[grade].sort();
   }
 
-  remove = (student) => {
-    for (const [grade, name] of Object.entries(this._db)) {
-      if (name == student) {
-        this._db[grade] = this._db[grade].filter((value) => value != student);
-        return;
-      }
-    }
+  grade(grade) {
+    return Array.from(this._db[grade] || []);
   }
 
-  copy = (data) => { return JSON.parse(JSON.stringify(data)) }
-
-  grade = (grade) => { return this._db[grade] ? this.copy(this._db[grade].sort()) : [] }
+  copy = (data) => {
+    return JSON.parse(JSON.stringify(data));
+  };
 }
