e3a074f4555fcafcddd8104a2124932bbb1aae90
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-08-23 16:31:56 +0200

    Ruby, resistor color duo uses a module defined map

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,17 +1,18 @@
 module ResistorColorDuo
+  TRANSLATE = {
+    'black' => '0',
+    'brown' => '1',
+    'red' => '2',
+    'orange' => '3',
+    'yellow' => '4',
+    'green' => '5',
+    'blue' => '6',
+    'violet' => '7',
+    'grey' => '8',
+    'white' => '9'
+  }.freeze
+
   def self.value(input)
-    translate = {
-      "black" => "0",
-      "brown" => "1",
-      "red" => "2",
-      "orange" => "3",
-      "yellow" => "4",
-      "green" => "5",
-      "blue" => "6",
-      "violet" => "7",
-      "grey" => "8",
-      "white" => "9",
-    }
-    input.take(2).map(&translate).join("").to_i
+    input.take(2).map(&TRANSLATE).join('').to_i
   end
 end
