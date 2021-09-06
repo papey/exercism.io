5d6eb9ad70bcd0f4c1f653919ef3d968d0a07186
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-09-06 09:41:11 +0200

    Ruby, add assembly line

---
+++ ruby/assembly-line/assembly_line.rb
@@ -0,0 +1,20 @@
+class AssemblyLine
+  attr_reader :speed, :success_rate
+
+  MINUTES_PER_HOUR = 60
+  CARS_PER_HOUR = 221
+  SUCCESS_RATES = { 1..4 => 1, 5..8 => 0.9, 9 => 0.8, 10 => 0.77 }.freeze
+
+  def initialize(speed)
+    @speed = speed
+    @success_rate = SUCCESS_RATES.select { |rate| rate === speed }.values.first
+  end
+
+  def production_rate_per_hour
+    speed * CARS_PER_HOUR * success_rate
+  end
+
+  def working_items_per_minute
+    (production_rate_per_hour / MINUTES_PER_HOUR).to_i
+  end
+end
