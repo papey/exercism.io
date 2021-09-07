e738ea17f73ec76126cc1b09eac5737bd6e21329
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-09-07 09:04:47 +0200

    Go, update allergies

--- go/allergies/allergies.go
+++ go/allergies/allergies.go
@@ -1,6 +1,67 @@
 package allergies
 
-var allergies = [8]string{"eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"}
+const (
+	eggs = 1 << iota
+	peanuts
+	shellfish
+	strawberries
+	tomatoes
+	chocolate
+	pollen
+	cats
+)
+
+const allergies = 8
+
+// Allergy indentify an allergy using an int code
+type Allergy uint
+
+func (a Allergy) String() string {
+	switch a {
+	case eggs:
+		return "eggs"
+	case peanuts:
+		return "peanuts"
+	case shellfish:
+		return "shellfish"
+	case strawberries:
+		return "strawberries"
+	case tomatoes:
+		return "tomatoes"
+	case chocolate:
+		return "chocolate"
+	case pollen:
+		return "pollen"
+	case cats:
+		return "cats"
+	default:
+		panic("unsupported allergy")
+	}
+}
+
+// NewAllergy maps string to an allergy value
+func NewAllergy(s string) Allergy {
+	switch s {
+	case "eggs":
+		return eggs
+	case "peanuts":
+		return peanuts
+	case "shellfish":
+		return shellfish
+	case "strawberries":
+		return strawberries
+	case "tomatoes":
+		return tomatoes
+	case "chocolate":
+		return chocolate
+	case "pollen":
+		return pollen
+	case "cats":
+		return cats
+	default:
+		panic("unsupported allergy")
+	}
+}
 
 // Result wraps substance and alergic result together
 type Result struct {
@@ -10,9 +71,9 @@ type Result struct {
 
 // Allergies is used to list all allergiges of the subject using it's score
 func Allergies(score uint) (result []string) {
-	for i, substance := range allergies {
-		if (score >> i & 1) == 1 {
-			result = append(result, substance)
+	for i := uint(0); i < allergies; i++ {
+		if score&(1<<i) == (1 << i) {
+			result = append(result, Allergy(1<<i).String())
 		}
 	}
 
@@ -21,11 +82,6 @@ func Allergies(score uint) (result []string) {
 
 // AllergicTo check if subject if allergic to a list of substances using it's score
 func AllergicTo(score uint, test string) bool {
-	for i, substance := range allergies {
-		if substance == test {
-			return (score >> i & 1) == 1
-		}
-	}
-
-	return false
+	a := NewAllergy(test)
+	return score&uint(a) == uint(a)
 }
