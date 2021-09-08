9d4b647c74ab2049e2f3b8c8c17fd34c1b3fe8b0
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-09-08 18:33:20 +0200

    TS, update secret handshake exercise

--- typescript/secret-handshake/secret-handshake.ts
+++ typescript/secret-handshake/secret-handshake.ts
@@ -1,28 +1,22 @@
-// Fefine an action to do when a code is received
+// Define an action to do when a code is received
 type action = (array: string[]) => string[];
 
-// A wrapper for a common used action
-function push(array: string[], value: string): string[] {
-  array.push(value);
-  return array;
-}
-
 const events: Map<number, action> = new Map([
-  [0b00001, (array: string[]): string[] => push(array, "wink")],
-  [0b00010, (array: string[]): string[] => push(array, "double blink")],
-  [0b00100, (array: string[]): string[] => push(array, "close your eyes")],
-  [0b01000, (array: string[]): string[] => push(array, "jump")],
+  [0b00001, (array: string[]): string[] => [...array, "wink"]],
+  [0b00010, (array: string[]): string[] => [...array, "double blink"]],
+  [0b00100, (array: string[]): string[] => [...array, "close your eyes"]],
+  [0b01000, (array: string[]): string[] => [...array, "jump"]],
   [0b10000, (array: string[]): string[] => array.reverse()],
 ]);
 
 export function commands(code: number): string[] {
-  let sequence: string[] = [];
-
-  events.forEach((action, flag) => {
-    if (code & flag) {
-      sequence = action(sequence);
-    }
-  });
-
-  return sequence;
+  return Array.from(events.entries()).reduce<string[]>(
+    (sequence, [flag, action]) => {
+      if (code & flag) {
+        return action(sequence);
+      }
+      return sequence;
+    },
+    new Array()
+  );
 }
