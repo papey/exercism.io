04aa887eceb8aa643701280ec0d91272b1fe395a
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-09-24 21:13:17 +0200

    Rust, low-power-embedeed-game: add solution

---
+++ rust/low-power-embedded-game/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (dividend / divisor, dividend % divisor)
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.enumerate().filter_map(|(i, e)| {
+        if i % 2 == 0 {
+            return Some(e);
+        }
+
+        return None;
+    })
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        self.0.abs() + self.1.abs()
+    }
+}
