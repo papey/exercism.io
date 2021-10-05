442a0c641bb48b97f2c900458c9e68f19a623d8a
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-10-05 16:59:26 +0200

    Rust, short-fib: add solution

---
+++ rust/short-fibonacci/src/lib.rs
@@ -0,0 +1,30 @@
+/// Create an empty vector
+pub fn create_empty() -> Vec<u8> {
+    Vec::new()
+}
+
+/// Create a buffer of `count` zeroes.
+///
+/// Applications often use buffers when serializing data to send over the network.
+pub fn create_buffer(count: usize) -> Vec<u8> {
+    vec![0; count]
+}
+
+const SIZE: usize = 5;
+
+/// Create a vector containing the first five elements of the Fibonacci sequence.
+///
+/// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
+/// Its first five elements are `1, 1, 2, 3, 5`.
+pub fn fibonacci() -> Vec<u8> {
+    let mut buffer = create_buffer(SIZE);
+
+    buffer[0] = 1;
+    buffer[1] = 1;
+
+    for i in 2..=SIZE - 1 {
+        buffer[i] = buffer[i - 2] + buffer[i - 1]
+    }
+
+    buffer
+}
