4254a6738412b7362ea7bc1940da1f944f43db8f
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-10-25 13:50:49 +0200

    Crystal, atbash-cipher: less audacious solution

--- crystal/atbash-cipher/src/atbash_cipher.cr
+++ crystal/atbash-cipher/src/atbash_cipher.cr
@@ -1,47 +1,34 @@
 NON_ALPHANUM_REGEX = /[^A-Za-z0-9]/
 
-struct Char
-  def atbash_encode
-    return self unless 'a' <= self <= 'z'
+A = 'a'.ord
+Z = 'z'.ord
 
-    ('a'.ord + ('z' - self)).chr
-  end
+SHIFT = A + Z
 
-  def atbash_decode
-    return self unless 'a' <= self <= 'z'
-
-    ('z'.ord - (self - 'a')).chr
-  end
-end
-
-class String
-  def atbash_encode
-    self
+module AtbashCipher
+  def self.encode(sentence)
+    sentence
       .gsub(NON_ALPHANUM_REGEX, "")
       .downcase
       .chars
-      .map(&.atbash_encode)
+      .map { |l| convert(l) }
       .in_groups_of(5).flat_map do |element|
       element.compact.join
     end.join(" ")
   end
 
-  def atbash_decode
-    self
-      .gsub(/\s/, "")
-      .downcase
-      .chars
-      .map(&.atbash_decode)
-      .join("")
-  end
-end
-
-module AtbashCipher
-  def self.encode(sentence)
-    sentence.atbash_encode
+  def self.convert(letter)
+    code = letter.ord
+    return letter if code < A || code > Z
+    (SHIFT - code).chr
   end
 
   def self.decode(sentence)
-    sentence.atbash_decode
+    sentence
+      .gsub(/\s/, "")
+      .downcase
+      .chars
+      .map { |l| convert(l) }
+      .join("")
   end
 end
