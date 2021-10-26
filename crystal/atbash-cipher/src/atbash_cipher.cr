c0a60059d3dc1033ebd2b06e238c0e34d7c089f0
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-10-26 10:17:09 +0200

    Crystal, atbash-cipher: more direct solution

--- crystal/atbash-cipher/src/atbash_cipher.cr
+++ crystal/atbash-cipher/src/atbash_cipher.cr
@@ -1,34 +1,23 @@
 NON_ALPHANUM_REGEX = /[^A-Za-z0-9]/
 
-A = 'a'.ord
-Z = 'z'.ord
-
-SHIFT = A + Z
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+ATBASH   = "zyxwvutsrqponmlkjihgfedcba"
 
 module AtbashCipher
   def self.encode(sentence)
     sentence
       .gsub(NON_ALPHANUM_REGEX, "")
       .downcase
+      .tr(ALPHABET, ATBASH)
       .chars
-      .map { |l| convert(l) }
-      .in_groups_of(5).flat_map do |element|
-      element.compact.join
-    end.join(" ")
-  end
-
-  def self.convert(letter)
-    code = letter.ord
-    return letter if code < A || code > Z
-    (SHIFT - code).chr
+      .in_groups_of(5).flat_map { |e| e.compact.join }
+      .join(" ")
   end
 
   def self.decode(sentence)
     sentence
       .gsub(/\s/, "")
       .downcase
-      .chars
-      .map { |l| convert(l) }
-      .join("")
+      .tr(ATBASH, ALPHABET)
   end
 end
