5c72e3cc0435184e3f487095555d547e510104ca
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-11-02 18:12:00 +0100

    JS, mixed-juices: add solution

---
+++ javascript/mixed-juices/mixed-juices.js
@@ -0,0 +1,73 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Determines how long it takes to prepare a certain juice.
+ *
+ * @param {string} name
+ * @returns {number} time in minutes
+ */
+export function timeToMixJuice(name) {
+  switch (name) {
+    case "Pure Strawberry Joy":
+      return 0.5;
+    case "Energizer":
+      return 1.5;
+    case "Green Garden":
+      return 1.5;
+    case "Tropical Island":
+      return 3;
+    case "All or Nothing":
+      return 5;
+    default:
+      return 2.5;
+  }
+}
+
+/**
+ * Calculates the number of limes that need to be cut
+ * to reach a certain supply.
+ *
+ * @param {number} wedgesNeeded
+ * @param {string[]} limes
+ * @returns {number} number of limes cut
+ */
+export function limesToCut(wedgesNeeded, limes) {
+  let toCut = 0;
+
+  while (wedgesNeeded > 0 && limes.length > 0) {
+    switch (limes.shift()) {
+      case "small":
+        wedgesNeeded -= 6;
+        break;
+      case "medium":
+        wedgesNeeded -= 8;
+        break;
+      case "large":
+        wedgesNeeded -= 10;
+        break;
+    }
+
+    toCut++;
+  }
+
+  return toCut;
+}
+
+/**
+ * Determines which juices still need to be prepared after the end of the shift.
+ *
+ * @param {number} timeLeft
+ * @param {string[]} orders
+ * @returns {string[]} remaining orders after the time is up
+ */
+export function remainingOrders(timeLeft, orders) {
+  while (timeLeft > 0) {
+    timeLeft -= timeToMixJuice(orders.shift());
+  }
+
+  return orders;
+}
