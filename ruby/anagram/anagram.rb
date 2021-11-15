4dec60f2eb4b5e3f6bceae7c4c87b43c1049595a
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-11-15 09:36:20 +0100

    Ruby, anagram: more idioms

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,11 +1,13 @@
 class Anagram
+  attr_reader :base, :letters
+
   def initialize(word)
     @base = word.downcase
     @letters = @base.chars.sort
   end
 
   def match(candidates)
-    candidates.reject { |candidate| candidate.downcase == @base }
-              .select { |candidate| @letters == candidate.downcase.chars.sort }
+    candidates.reject { |candidate| candidate.downcase == base }
+              .select { |candidate| letters == candidate.downcase.chars.sort }
   end
 end
