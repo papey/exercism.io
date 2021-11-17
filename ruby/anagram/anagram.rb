745915fb0ccb9523ec65414c5a0a4e57dafa341b
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-11-17 09:24:57 +0100

    Ruby, anagram: playing with lambdas

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,19 +1,19 @@
 class Anagram
   private
 
-  attr_reader :is_anagram, :is_same_word
+  attr_reader :rejections, :selections
 
   def initialize(word)
     base = word.downcase
     letters = base.chars.sort
-    @is_same_word = ->(candidate) { candidate.downcase == base }
-    @is_anagram = ->(candidate) { letters == candidate.downcase.chars.sort }
+    @rejections = ->(candidate) { candidate.downcase == base }
+    @selections = ->(candidate) { letters == candidate.downcase.chars.sort }
   end
 
   public
 
   def match(candidates)
-    candidates.reject(&is_same_word)
-              .select(&is_anagram)
+    candidates.reject(&rejections)
+              .select(&selections)
   end
 end
