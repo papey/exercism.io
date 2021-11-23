1fd5877db919bfa220eefa1dc719ebbcecaea7bc
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-11-23 08:56:21 +0100

    Nim, anagram: more idioms

--- nim/anagram/anagram.nim
+++ nim/anagram/anagram.nim
@@ -1,8 +1,8 @@
-import sequtils
-import algorithm
-import strutils
+import sequtils, algorithm, strutils
 
 proc detectAnagrams*(word: string, candidates: seq[string]): seq[string] =
-    let lowered = word.toLower
-    let letters_pool = lowered.sorted
+    let
+        lowered = word.toLower
+        letters_pool = lowered.sorted
+
     result = candidates.filterIt(it.toLower != lowered and it.toLower.sorted == letters_pool)
