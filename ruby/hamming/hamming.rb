ba74613294667cae29e514fa2ad778d9fadd8cdc
Wilfried OLLIVIER <wollivier@fdn.fr>
2021-11-29 11:00:54 +0100

    Ruby, hamming: go simple

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,6 +2,6 @@ module Hamming
   def self.compute(strand_a, strand_b)
     raise ArgumentError, 'strand length mismatch' unless strand_a.length == strand_b.length
 
-    strand_a.each_char.zip(strand_b.each_char).each.count { |l1, l2| l1 != l2 }
+    strand_a.chars.zip(strand_b.chars).count { |l1, l2| l1 != l2 }
   end
 end
