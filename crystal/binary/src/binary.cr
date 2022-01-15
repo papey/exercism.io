1170d92919b3971b3aabd41645881dc9b636dd1e
Wilfried OLLIVIER <wollivier@fdn.fr>
2022-01-15 16:48:35 +0100

    Crystal, binary: simplify

--- crystal/binary/src/binary.cr
+++ crystal/binary/src/binary.cr
@@ -1,7 +1,8 @@
-module Binary
+class Binary
   def self.to_decimal(binary : String) : Int
+    raise ArgumentError.new("Invalid binary string") unless binary.match(/^[0-1]+$/)
+
     binary.reverse.chars.each_with_index.sum do |char, index|
-      raise ArgumentError.new("Invalid binary string") unless char == '0' || char == '1'
       char.to_i << index
     end
   end
