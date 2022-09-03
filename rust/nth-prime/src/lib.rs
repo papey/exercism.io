4325e8d946e488b9c6f1b3b3a4fc7b1e72c571f5
Wilfried OLLIVIER <wollivier@fdn.fr>
2022-09-03 19:20:16 +0200

    Rust, nth-prime: optimize solution

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,7 +1,22 @@
 pub fn nth(n: u32) -> u32 {
-    (2..).filter(|c| is_prime(*c)).nth(n as usize).unwrap()
+    match n {
+        0 => 2,
+        1 => 3,
+        _ => prime(n),
+    }
 }
 
-fn is_prime(n: u32) -> bool {
-    !(2..n - 1).any(|i| n % i == 0)
+fn prime(n: u32) -> u32 {
+    let mut primes = vec![2, 3];
+    let mut next = *primes.last().unwrap_or(&0) + 2;
+
+    while primes.len() <= n as usize {
+        if primes.iter().all(|&i| next % i != 0) {
+            primes.push(next)
+        }
+
+        next += 2;
+    }
+
+    *primes.last().unwrap_or(&0)
 }
