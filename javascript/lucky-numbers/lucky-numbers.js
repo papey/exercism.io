74a23e0bec7e3b452bc5a6fa7ca592076432753c
Wilfried OLLIVIER <wollivier@fdn.fr>
2023-01-08 22:35:12 +0100

    JavaScript, lucky-numbers: add solution

---
+++ javascript/lucky-numbers/lucky-numbers.js
@@ -0,0 +1,41 @@
+// @ts-check
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+export function twoSum(array1, array2) {
+  return array1.reduce(toNumReducer) + array2.reduce(toNumReducer);
+}
+
+function toNumReducer(acc, v) {
+  return acc * 10 + v;
+}
+
+/**
+ * Checks whether a number is a palindrome.
+ *
+ * @param {number} value
+ * @returns {boolean} whether the number is a palindrome or not
+ */
+export function luckyNumber(value) {
+  return value.toString() === value.toString().split("").reverse().join("");
+}
+
+/**
+ * Determines the error message that should be shown to the user
+ * for the given input value.
+ *
+ * @param {string|null|undefined} input
+ * @returns {string} error message
+ */
+export function errorMessage(input) {
+  if (!input) {
+    return "Required field";
+  }
+
+  return Number(input) ? "" : "Must be a number besides 0";
+}
