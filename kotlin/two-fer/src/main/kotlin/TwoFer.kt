6136b1dd66536a6ec84cf17c2a1d50bb430650ca
Wilfried OLLIVIER <wollivier@fdn.fr>
2023-10-03 20:03:22 +0200

    Kotlin, twofer: add solution

---
+++ kotlin/two-fer/src/main/kotlin/TwoFer.kt
@@ -0,0 +1,3 @@
+fun twofer() = "One for you, one for me."
+
+fun twofer(name: String = "you"): String = "One for $name, one for me."
---
+++ kotlin/two-fer/src/test/kotlin/TwoFerTest.kt
@@ -0,0 +1,25 @@
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class TwoFerTest {
+
+    @Test
+    fun noNameGiven() {
+        assertEquals("One for you, one for me.", twofer())
+    }
+
+    @Test
+    fun aNameGiven() {
+        assertEquals("One for Alice, one for me.", twofer("Alice"))
+    }
+
+    @Test
+    fun anotherNameGiven() {
+        assertEquals("One for Bob, one for me.", twofer("Bob"))
+    }
+
+    @Test
+    fun emptyStringGiven() {
+        assertEquals("One for , one for me.", twofer(""))
+    }
+}
